2395c0734273e0e7ee53e37ed5ee32ce1ce2ad1a
Phillip Dang <dangthanhphat23031997@gmail.com>
2021-07-20 20:48:31 +0700

    First commit

---
+++ SignUpViaGoogle/App_Start/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.MicrosoftAccount;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignUpViaGoogle.App_Start.Startup))]
+
+namespace SignUpViaGoogle.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                LoginPath = new PathString("/Home/Index"),
+                SlidingExpiration = true
+            });
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "137750893632-ek3n827krshg2obueusqkemm1c0jqgv1.apps.googleusercontent.com",
+                ClientSecret = "DOX3mSg2YUPBSetJLC18zyWy"
+            });
+            app.UseMicrosoftAccountAuthentication(new MicrosoftAccountAuthenticationOptions()
+            {
+                ClientId = "59e4935f-a0db-4321-838e-8cc98da5284b",
+                ClientSecret = "-Ox.7nS85joRzq~j_.Atm1x~5QGj51Avg3"
+            });
+        }
+    }
+}
---
+++ SignUpViaGoogle/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using SignUpViaGoogle.Models;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignUpViaGoogle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void SignIn(string ReturnUrl = "/", string type = "")
+        {
+            if (!Request.IsAuthenticated)
+            {
+                if (type == "Google")
+                {
+                    HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "Home/GoogleLoginCallback" }, "Google");
+                }
+                
+                if(type == "Microsoft")
+                {
+                    HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "Home/GoogleLoginCallback" }, "Microsoft");
+                }
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult GoogleLoginCallback()
+        {
+            var claimsPrincipal = HttpContext.User.Identity as ClaimsIdentity;
+
+            var loginInfo = GoogleLoginViewModel.GetLoginInfo(claimsPrincipal);
+            if (loginInfo == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            //WebEntities db = new WebEntities(); //DbContext
+            //var user = db.UserAccounts.FirstOrDefault(x => x.Email == loginInfo.emailaddress);
+
+            //if (user == null)
+            //{
+            //    user = new UserAccount
+            //    {
+            //        Email = loginInfo.emailaddress,
+            //        GivenName = loginInfo.givenname,
+            //        Identifier = loginInfo.nameidentifier,
+            //        Name = loginInfo.name,
+            //        SurName = loginInfo.surname,
+            //        IsActive = true
+            //    };
+            //    db.UserAccounts.Add(user);
+            //    db.SaveChanges();
+            //}
+
+            var ident = new ClaimsIdentity(
+                    new[] { 
+									// adding following 2 claim just for supporting default antiforgery provider
+									new Claim(ClaimTypes.NameIdentifier, loginInfo.emailaddress),
+                                    new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider", "ASP.NET Identity", "http://www.w3.org/2001/XMLSchema#string"),
+
+                                    new Claim(ClaimTypes.Name, loginInfo.name),
+                                    new Claim(ClaimTypes.Email, loginInfo.emailaddress),
+									// optionally you could add roles if any
+									new Claim(ClaimTypes.Role, "User")
+                    },
+                    CookieAuthenticationDefaults.AuthenticationType);
+
+
+            HttpContext.GetOwinContext().Authentication.SignIn(
+                        new AuthenticationProperties { IsPersistent = false }, ident);
+            return Redirect("~/");
+
+        }
+
+        public ActionResult SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            return Redirect("~/");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
